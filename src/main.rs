221d83b8e0e2c3d90c54f35560e1a279dd01c986
Reece Rose <reecerose18@gmail.com>
2021-06-13 11:41:28 -0400

    Now multiple clients can connect and send messages

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,22 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-
-    let (read, mut writer) = socket.split();
-    let mut reader = BufReader::new(read);
-    let mut line = String::new();
-
     loop {
-        let bytes_read = reader.read_line(&mut line).await.unwrap();
-        if bytes_read == 0 {
-            break; // when client disconnects, quit program
-        }
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            let (read, mut writer) = socket.split();
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
 
-        writer.write_all(&line.as_bytes()).await.unwrap();
-        line.clear();
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break; // when client disconnects, quit program
+                }
+
+                writer.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
 }
