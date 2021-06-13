87293521a27628126b85fabf74bfefb2cffd8eec
Reece Rose <reecerose18@gmail.com>
2021-06-13 12:00:02 -0400

    Messages are now being sent to all connected clients. Duplicate 'echo' message not sent to sender.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,43 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel(10);
+
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
         tokio::spawn(async move {
             let (read, mut writer) = socket.split();
             let mut reader = BufReader::new(read);
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break; // when client disconnects, quit program
-                }
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break; // when client disconnects, quit this loop
+                        }
 
-                writer.write_all(&line.as_bytes()).await.unwrap();
-                line.clear();
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+                        if addr != other_addr {
+                            writer.write_all(&msg.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
             }
         });
     }
